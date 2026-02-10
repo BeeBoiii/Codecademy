2a51afb5cd6444b4debe7593909b03a943ba6e84
Millie Ren <renyifei2017@hotmail.com>
2026-02-11 00:31:33 +0800

    build basic search bar and test in app.jsx

--- (react) Jammming/Jammming/src/components/SearchBar/searchBar.jsx
+++ (react) Jammming/Jammming/src/components/SearchBar/searchBar.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState } from "react"
 
 function SearchBar() {
-    const [userInput, setUserInput] = useState('');
+    const [userInput, setUserInput] = useState('')
 
     return (
         <form>
             <input 
                 placeholder="Enter a song name" 
                 name="searchBar" 
-                type="text" 
+                type="search" 
                 value={userInput} 
                 onChange={(e) => setUserInput(e.target.value)}
             />
-            
+            <button>Search</button>
         </form>
     )
 }
