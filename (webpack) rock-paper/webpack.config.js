d0b8cff60142e747fb1a9a81943e2ad9441b8eaf
Millie Ren <renyifei2017@hotmail.com>
2026-05-09 17:28:11 +0800

    import path in config file

--- (webpack) rock-paper/webpack.config.js
+++ (webpack) rock-paper/webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: './code/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, dist)
+        path: path.resolve(__dirname, 'dist')
     },
     devSever: {
         port: 4001,
